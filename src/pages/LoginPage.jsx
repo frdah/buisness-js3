8d76fbb66c952c7c03d70b4c5f8e23282cbeb112
Frida <frida.johansson888@gmail.com>
2020-09-28 21:03:42 +0200

    Add login error message

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react'
 import {useHistory} from "react-router-dom"
 import UserKit from '../data/UserKit'
-import {DivCenterColumn, H2, LoginButton, CreateCustomerInput, NavButton} from "../components/styles"
+import {DivCenterColumn, H2, LoginButton, CreateCustomerInput, NavButton, P} from "../components/styles"
 
 export default function LoginPage() {
 
     const [loginEmail, setLoginEmail] = useState("frida.johansson888+1@gmail.com")
     const [loginPassword, setLoginPassword] = useState("js-fend-19")
+    const [showError, setShowError] = useState(null)
     
     const userKit = new UserKit()
     const history = useHistory()
@@ -15,9 +16,11 @@ export default function LoginPage() {
         userKit.login(loginEmail, loginPassword)
         .then(res => res.json())
         .then(data => {
+            if (data.token === undefined) {return setShowError("Wrong input, please try again")}
           userKit.setToken(data.token)
           history.push("/home")
         }) 
+        
     }
 
     function handlePushRegister() {history.push("/")}
@@ -27,6 +30,7 @@ export default function LoginPage() {
             <H2>Login</H2>
             <CreateCustomerInput placeholder="email" value={loginEmail} onChange={e => setLoginEmail(e.target.value)}/>
             <CreateCustomerInput type="password" placeholder="password" value={loginPassword} onChange={e => setLoginPassword(e.target.value)}/>
+            {showError && <P warning>{showError}</P>}
             <LoginButton onClick={handleLogin}>Login</LoginButton>
             <NavButton onClick={handlePushRegister}>Register</NavButton>
         </DivCenterColumn>
