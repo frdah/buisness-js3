52b1f889317b6761d31432f7820979b1aaa82681
Frida <frida.johansson888@gmail.com>
2020-09-29 10:48:39 +0200

    Updates

---
+++ precache-manifest.79f9f175c1173a965fb7dd48c3312681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299f8661935ec9e5cc1af946a31509e5",
+    "url": "//frdah.github.io/buisness-js3/index.html"
+  },
+  {
+    "revision": "91416e0c4b3de69ba300",
+    "url": "//frdah.github.io/buisness-js3/static/js/2.a37d049e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//frdah.github.io/buisness-js3/static/js/2.a37d049e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30755392499a13e3989b",
+    "url": "//frdah.github.io/buisness-js3/static/js/main.d4683490.chunk.js"
+  },
+  {
+    "revision": "48ee92f0cfd9ea894e27",
+    "url": "//frdah.github.io/buisness-js3/static/js/runtime-main.1c749c33.js"
+  }
+]);
